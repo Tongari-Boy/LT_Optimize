8e9c3d89599472b94e253f3915f2594dbcc174de
Tongari-Boy <tongariboy@outlook.com>
2026-01-30 07:58:34 +0900

    first commit

---
+++ components/global-bottom.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- ページ番号（右下） -->
+  <footer class="absolute bottom-0 right-0 p-2 text-xs text-gray-500">
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+  </footer>
+  
+  <!-- コピーライト透かし（左下） -->
+  <footer class="absolute bottom-0 left-0 p-2 text-xs text-gray-400 opacity-70">
+    © 2025 Kenta Kuzuhara
+  </footer>
+</template>
